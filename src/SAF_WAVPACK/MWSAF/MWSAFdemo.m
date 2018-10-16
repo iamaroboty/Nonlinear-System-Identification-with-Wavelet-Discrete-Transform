43f331c5cfe0bb7ecbbaeee843f225689f4d037f
Andrea <a.castellani93@gmail.com>
2018-10-16 15:49:18 +0200

    Added Speech signal

--- src/SAF_WAVPACK/MWSAF/MWSAFdemo.m
+++ src/SAF_WAVPACK/MWSAF/MWSAFdemo.m
@@ -8,13 +8,13 @@ clear all;
 
 % Adaptive filter parameters
 mu = 0.1;                      % Step size
-M = 256;                       % Length of unknown system response
+M = 1024;                       % Length of unknown system response
 level = 2;                     % Levels of Wavelet decomposition
-filters = 'db2';               % Set wavelet type
+filters = 'db16';               % Set wavelet type
 Q =0;
 
 % Run parameters
-iter = 1.0*80000;                % Number of iterations
+iter = 2.0*80000;                % Number of iterations
 b = load('h1.dat');              % Unknown system (select h1 or h2)
 b = b(1:M);                      % Truncate to length M
 
@@ -38,20 +38,21 @@ b = b(1:M);                      % Truncate to length M
 % k = 2*a/(1-a);
 % b = (1+k)*(b)./(1+k*abs(b));
 
-%  %% load reverb 
+ %% load reverb 
 %  [y,Fs] = audioread('reverb_shimmer.wav');
 %  yr = resample(y, 1, 100);
-%  b = yr(510:510-1+M,1);
+%  b = yr(500:500-1+M,1);
 
 %%
 tic;
 % Adaptation process
 fprintf('Wavelet type: %s, levels: %d, step size = %f \n', filters, level, mu);
-[un,dn,vn] = GenerateResponses(iter,b,sum(100*clock),1,40); %iter, b, seed, ARtype, SNR
-S = SWAFinit(M, mu, level, filters); 
-% S = MWSAFinit(M,mu,level,filters,Q);
+% [un,dn,vn] = GenerateResponses(iter,b,sum(100*clock),2,40); %iter, b, seed, ARtype, SNR
+[un,dn,vn] = GenerateResponses_speech(b,'SpeechSample.mat');
+% S = SWAFinit(M, mu, level, filters); 
+S = MWSAFinit(M,mu,level,filters,Q);
 S.unknownsys = b; 
-[en, S] = MWSAFadapt_v2(un, dn, S);                 % Perform WSAF Algorithm 
+[en, S] = MWSAFadapt(un, dn, S);                 % Perform WSAF Algorithm 
 
 EML = S.eml.^2;                  % System error norm (normalized)
 err_sqr = en.^2;
@@ -61,7 +62,7 @@ fprintf('Total time = %.3f mins \n',toc/60);
 figure;         % Plot MSE
 q = 0.99; MSE = filter((1-q),[1 -q],err_sqr);
 hold on; plot((0:length(MSE)-1)/1024,10*log10(MSE));
-axis([0 iter/1024 -60 10]);
+% axis([0 iter/1024 -60 10]);
 xlabel('Number of iterations (\times 1024 input samples)'); 
 ylabel('Mean-square error (with delay)'); grid on;
 fprintf('MSE = %.2f dB\n', mean(10*log10(MSE(end-2048:end))))
