4d117d98a53cf986593df68fcf6486c735c819a1
popcornell <cornellsamuele@gmail.com>
2018-10-22 20:02:28 +0200

    volterra 2nd matrix prova

--- src/SAF_WAVPACK/MWSAF/Volterra MSWAF/Volterra_2ord_adapt_matrix.m
+++ src/SAF_WAVPACK/MWSAF/Volterra MSWAF/Volterra_2ord_adapt_matrix.m
@@ -32,21 +32,32 @@ S.synthesis_bank = F;
 U1_tot = zeros(S.length(1),2^level); % output of analysis filters for 1st order signal 
 
 for k = 1:S.length(2)
-U2_tot{k} = zeros(S.length(2),2^level); % output of analysis filters for 2nd order signal 
+if S.length(2)-k+1 >= len   
+U2_tot{k} = zeros(S.length(2)-k+1,2^level); % output of analysis filters for 2nd order signal 
+else
+U2_tot{k} = zeros(len,2^level);
+end
 end
 
 a1 = zeros(len,1);
-a2 = zeros(len, S.length(2));
+a2 = zeros(len, 1, S.length(2));
 d = zeros(len,1); 
 A1 = zeros(len,2^level);  
-A2 = zeros(len,S.length(2), 2^level);
+A2 = zeros(len, 2^level, S.length(2));
 
 un2_tap = zeros(S.length(2), 1); 
 
 z = zeros(len,1);
 
 w1 = zeros(S.length(1),1);
-w2 = zeros(S.length(2),1);
+
+for k = 1:S.length(2)
+if S.length(2)-k+1 >= len    
+w2{k} = zeros(S.length(2)-k+1, 1); 
+else
+w2{k} = zeros(len, 1);
+end
+end
 
 ITER = length(un);
 en = zeros(1,ITER);               % Initialize error sequence to zero
@@ -67,8 +78,8 @@ for n = 1:ITER
     a1 = [un(n); a1(1:end-1)];                       % Update tapped-delay line of u(n)
     A1 = [a1, A1(:,1:end-1)];                         % Update buffer
     
-    a2 = cat(1, (un(n).*un2_tap)', a2(1:end-1,:));                       % Update tapped-delay line of u(n)
-    A2 = cat(3, a2, A2(:,:,1:end-1));                         % Update buffer
+    a2 = cat(1, reshape(un2_tap.*un(n),[1,1,S.length(2)]), a2(1:end-1,:,:));                 % Update tapped-delay line of u(n)
+    A2 = cat(2, a2, A2(:,1:end-1,:));                         % Update buffer
    
        
     if (mod(n,2^level)==0)                               % Tap-weight adaptation at decimated rate
@@ -76,25 +87,30 @@ for n = 1:ITER
         U1_2 = U1_tot(1:end-2^level,:);
         U1_tot = [U1', U1_2']';                           % Subband data matrix
         
+        second_order = 0; 
         
         for k= 1:S.length(2)
-        U2{k} = (H'*A2(:,k,:))';                              % Partitioning u(n) 
+        U2{k} = (H'*A2(:,:,k))';                              % Partitioning u(n) 
         U2_2{k} = U2_tot{k}(1:end-2^level,:);
         U2_tot{k} = [U2{k}', U2_2{k}']';                           % Subband data matrix
+        
+        second_order  = second_order + U2_tot{k}'*w2{k}; 
+        
         end
         
         dD = H'*d;                                 % Partitioning d(n) 
         
-        eD = dD - U1_tot'*w1- U2_tot'*w2;                         % Error estimation
         
-        for k = 1:S.length(2)
-           eD = eD - U2_tot;
-            
-        end
+        
+        eD = dD - U1_tot'*w1- second_order;                          % Error estimation
+        
         
         if n >= AdaptStart
             w1 = w1 + U1_tot*(eD./(sum(U1_tot.*U1_tot)+alpha)')*mu(1); % Tap-weight adaptation
-            w2 = w2 + U2_tot*(eD./(sum(U2_tot.*U2_tot)+alpha)')*mu(2);
+            
+            for k = 1:S.length(2)
+            w2{k} = w2{k} + U2_tot{k}*(eD./(sum(U2_tot{k}.*U2_tot{k})+alpha)')*mu(2);
+            end
             
         end
         z = F*eD + z;                                       
