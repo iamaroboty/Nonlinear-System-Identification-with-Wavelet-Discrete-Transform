cca2bdd31705f9a239ba908d4f4eb2650eaa6f9c
popcornell <cornellsamuele@gmail.com>
2018-10-25 16:00:05 +0200

    multiple filter fullband NLMS

--- src/Wavelet Volterra Filtering/Volterra NLMS fullband/Volterra_NLMS_fullband_demo.m
+++ src/Wavelet Volterra Filtering/Volterra NLMS fullband/Volterra_NLMS_fullband_demo.m
@@ -9,10 +9,10 @@ close all;
 %% Unidentified System parameters
 order = 2; 
 M1 = 256; % length of first order volterra kernel
-M2 = 32; % length of second order volterra kernel
+M2 = 8; % length of second order volterra kernel
 
 NL_system.M = [M1, M2];
-gains = [1 0.5];
+gains = [1 1];
 
 %NL_system = create_volterra_sys(order, M, gains, 'nlsys1'); 
 %% Just a Delta
@@ -94,90 +94,31 @@ colorbar
 grid on;
 
 
-%% Adaptive filter parameters
-mu = [0.1, 0.01];                 % Step sizes for different kernels 
-
-level = [2];                  % Levels of Wavelet decomposition for different kernels
-filters = 'db2';               % Set wavelet type for different kernels
-
-% Run parameters
-iter = 2.0*80000;                % Number of iterations
-
-%%
-tic;
-% Adaptation process
-fprintf('Wavelet type: %s, levels: %d, step size = %f \n', filters, level, mu);
-[un,dn,vn] = GenerateResponses_Volterra(iter, NL_system ,sum(100*clock),1,40); %iter, b, seed, ARtype, SNR
-% [un,dn,vn] = GenerateResponses_speech_Volterra(NL_system,'SpeechSample.mat');
-
-%% Nonlinear model 
-S = Volterra_Init(NL_system.M, mu, level, filters); 
-
-% [en, S] = Volterra_2ord_adapt(un, dn, S);     
-% [en, S] = Volterra_2ord_adapt_shift(un, dn, S, shift);   
-[en, S] = Volterra_2ord_adapt_v2(un, dn, S, 10);
-
-
-err_sqr = en.^2;
-    
-fprintf('Total time = %.3f mins \n',toc/60);
-
-figure;         % Plot MSE
-q = 0.99; MSE = filter((1-q),[1 -q],err_sqr);
-hold on; plot((0:length(MSE)-1)/1024,10*log10(MSE), 'DisplayName', 'WavTerra');
-axis([0 iter/1024 -60 10]);
-xlabel('Number of iterations (\times 1024 input samples)'); 
-ylabel('Mean-square error (with delay)'); grid on;
-fprintf('MSE = %.2f dB\n', mean(10*log10(MSE(end-2048:end))))
-
-
 %% Fullband Volterra NLMS
 fprintf('-------------------------------------------------------------\n');
 fprintf('FULLBAND VOLTERRA NLMS\n');
-mu = [0.3, 0.1];
-Sfull = Volterra_NLMS_init(NL_system.M, mu); 
 
-[en, Sfull] = Volterra_NLMS_adapt_2(un, dn, Sfull);     
+% Run parameters
+iter = 2.0*80000;                % Number of iterations
+mu = [0.1, 0.1];
+C=1;
+
+
+Sfull = Volterra_NLMS_init(NL_system.M, mu); 
+[un,dn,vn] = GenerateResponses_Volterra(iter, NL_system ,sum(100*clock),1,40);
+
+[en, Sfull] = Volterra_NLMS_adapt_2(un, dn, Sfull, C);     
 
 err_sqr_full = en.^2;
     
 fprintf('Total time = %.3f mins \n',toc/60);
 
 % Plot MSE
+figure;
 q = 0.99; MSE_full = filter((1-q),[1 -q],err_sqr_full);
-hold on; plot((0:length(MSE_full)-1)/1024,10*log10(MSE_full), 'DisplayName', 'FB NLMS');
+plot((0:length(MSE_full)-1)/1024,10*log10(MSE_full), 'DisplayName', 'FB NLMS');
 axis([0 iter/1024 -60 10]);
 xlabel('Number of iterations (\times 1024 input samples)'); 
 ylabel('Mean-square error (with delay)'); grid on;
 fprintf('MSE = %.2f dB\n', mean(10*log10(MSE_full(end-2048:end))))
 legend('show');
-
-
-% %% linear model
-% 
-% fprintf('-----------------------------------------------------------\n');
-% fprintf('LINEAR MODEL\n');
-% 
-% 
-% S = SWAFinit(M1, mu(1), level(1), filters); 
-% [en, S] = MWSAFadapt(un, dn, S); 
-% 
-% err_sqr = en.^2;
-%     
-% fprintf('Total time = %.3f mins \n',toc/60);
-% 
-% figure;         % Plot MSE
-% q = 0.99; MSE = filter((1-q),[1 -q],err_sqr);
-% hold on; plot((0:length(MSE)-1)/1024,10*log10(MSE));
-% axis([0 iter/1024 -60 10]);
-% xlabel('Number of iterations (\times 1024 input samples)'); 
-% ylabel('Mean-square error (with delay)'); grid on;
-% fprintf('MSE = %.2f dB\n', mean(10*log10(MSE(end-2048:end))))
-
-
-
-% figure;                          % Plot misalignment
-% hold on; plot((0:length(EML)-1)/1024,10*log10(EML));
-% xlabel('Number of iterations (\times 1024 input samples)'); 
-% ylabel('Misalignment (dB)');
-% grid on;
